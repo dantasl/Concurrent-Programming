2acf20d5bace95666d4adec963f6213f34e8c496
Lucas Dantas <lucas_gomesdantas@hotmail.com>
2020-07-18 23:13:28 -0300

    Adding core methods.

--- List-Synchronization/src/com/vertumno/list/synchronization/SharedList.java
+++ List-Synchronization/src/com/vertumno/list/synchronization/SharedList.java
@@ -1,26 +1,32 @@
 package com.vertumno.list.synchronization;
 
+/**
+ * @author Lucas Gomes Dantas
+ */
+
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class SharedList {
 	private int size;
-	private Queue<Integer> list;
+	private LinkedList<Integer> list;
 	
 	public SharedList(int size) {
 		this.size = size;
-		list = new LinkedList<Integer>();
+		list = new LinkedList<>();
 	}
 	
 	public synchronized void search(int item) {
 		System.out.println(Thread.currentThread().getName() + " is searching for " + item);
+		list.contains(item);		
 	}
 	
 	public synchronized void insert(int item) {
 		System.out.println(Thread.currentThread().getName() + " is inserting " + item);
+		list.add(item);
 	}
 	
 	public synchronized void remove(int item) {
 		System.out.println(Thread.currentThread().getName() + " is removing " + item);
+		list.remove(item);
 	}
 }
